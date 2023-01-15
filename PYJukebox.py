d6c3f82743f9c1c6035e2662fb5af628832b4c9d
alex feliow <98061642+DED0026@users.noreply.github.com>
2023-01-15 10:12:44 -0600

    shut the fuck upinator
    yeah i added some more changes

--- PYJukebox.py
+++ PYJukebox.py
@@ -9,6 +9,10 @@ intents.message_content = True
 
 client = discord.Bot()
 
+#shut the fuck upinator changes here
+
+stfu = True;
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -17,7 +21,7 @@ async def on_ready():
 async def play(ctx, filename: str):
     channel = ctx.author.voice.channel
     if channel is None:
-        await ctx.respond("You are not in a voice channel.")
+        await ctx.respond("You are not in a voice channel.", ephemeral = stfu)
         return
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -33,35 +37,34 @@ async def play(ctx, filename: str):
             await ctx.send("No video found.")
             return
         url = result['entries'][0]['url']
-        # title = result['entries'][0]['title']
 
     # Connect to voice channel and play music
     
     if not channel.guild.voice_client:
         await channel.connect()
-    # await ctx.respond("playing: "+title)
+    await ctx.respond("playing something that matched: ^^^", ephemeral = stfu)
     voice = channel.guild.voice_client
     voice.play(discord.FFmpegPCMAudio(url))
     voice.source = discord.PCMVolumeTransformer(voice.source)
     voice.source.volume = 0.2
 
 @client.slash_command()
-async def set_volume(ctx, volume: float):
+async def volume(ctx, volume: float):
     if volume < 0 or volume > 100:
-        await ctx.respond("Volume must be between 0 and 100.")
+        await ctx.respond("Volume must be between 0 and 100.", ephemeral = stfu)
         return
     voice_client = ctx.guild.voice_client
     if not voice_client:
-        ctx.respond("I am not currently playing any music.")
+        await ctx.respond("I am not currently playing any music.", ephemeral = stfu)
         return
     voice_client.source.volume = volume/100
-    await ctx.respond(f"Volume set to {volume}%.")
+    await ctx.respond(f"Volume set to {volume}%.", ephemeral = stfu)
     return
 
 @client.slash_command()
 async def join(ctx):
     channel = ctx.author.voice.channel
-    await ctx.respond("Joining: "+str(ctx.author.voice.channel))
+    await ctx.respond("Joining: "+str(ctx.author.voice.channel), ephemeral = stfu)
     if not channel.guild.voice_client:
         await channel.connect()
 
@@ -70,7 +73,7 @@ async def disconnect(ctx):
     channel = ctx.author.voice.channel
     vc = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
     voice_client = channel.guild.voice_client
-    await ctx.respond("Disconnected From: "+str(ctx.author.voice.channel))
+    await ctx.respond("Disconnected From: "+str(ctx.author.voice.channel), ephemeral = stfu)
     voice_client.stop()
     await vc.disconnect()
     print("Bot disconnected")
@@ -80,25 +83,27 @@ async def pause(ctx):
     print("resume")
     voice_client = ctx.guild.voice_client
     if not voice_client:
-        return await ctx.respond("I am not currently playing any music.")
+        return await ctx.respond("I am not currently playing any music.", ephemeral = stfu)
     if voice_client.is_paused():
-        return await ctx.respond("The music is already paused.")
+        return await ctx.respond("The music is already paused.", ephemeral = stfu)
     voice_client.pause()
-    await ctx.respond("Paused the current music.")
+    await ctx.respond("Paused the current music.", ephemeral = stfu)
 
 @client.slash_command()
 async def stop(ctx):
     ctx.voice_client.stop()
+    await ctx.respond("stopped.", ephemeral = stfu)
+
 
 @client.slash_command()
 async def resume(ctx):
     print("pasue")
     voice_client = ctx.guild.voice_client
     if not voice_client:
-        return await ctx.respond("I am not currently playing any music.")
+        return await ctx.respond("I am not currently playing any music.", ephemeral = stfu)
     if not voice_client.is_paused():
-        return await ctx.respond("The music is not paused.")
+        return await ctx.respond("The music is not paused.", ephemeral = stfu)
     voice_client.resume()
-    await ctx.respond("Resumed the current music.")
+    await ctx.respond("Resumed the current music.", ephemeral = stfu)
 
 client.run('tokenere')
