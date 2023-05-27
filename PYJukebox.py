18e208a6cbc715b409e77d33b9ab7000ccbd6eca
BonesMgee <111199847+BonesMgee@users.noreply.github.com>
2023-05-27 11:08:55 +0100

    Update PYJukebox.py
    Change To Discord.py rather than Pycord, and fix a myriad of issues

--- PYJukebox.py
+++ PYJukebox.py
@@ -1,28 +1,86 @@
 import discord
-import youtube_dl
+import yt_dlp
+import ffmpeg
 import asyncio
 import time
-import os
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Bot(intents = intents)
+from discord import app_commands
+import json
 
 #shut the fuck upinator changes here
 
-stfu = True;
+stfu = True
+merf_count = 0
+
+intents = discord.Intents.all()
+client = discord.Client(intents = intents)
+
+class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    def __init__(self, person, **kwargs):
+        self.person = person
+        super().__init__(**kwargs)
+    
+    @discord.ui.button(label="Verify", style=discord.ButtonStyle.primary, emoji="😎") # Create a button with the label "😎 Click me!" with color Blurple
+    async def button_callback(self, interaction, button):
+        await interaction.response.send_message("You Are Now Verified!")
+        await self.person.add_roles(self.person.guild.get_role(1061340808445431920))
+
+@client.event
+async def on_message(message):
+    global merf_count
+    # Check if the message author has a role called "nerd"
+    if message.author.bot:
+        return
+    if "nerd" in [role.name.lower() for role in message.author.roles]:
+        await message.add_reaction("🤓")
+    if "merf" in message.content.lower():
+        merf_count += 1
+        if merf_count > 2:
+            await message.delete()
+            await message.channel.send("No M**f")
+    
+
+@client.event
+async def on_member_join(person):
+    await person.send(f"Thanks for Joining {person.guild.name}", view=MyView(person))
 
 @client.event
 async def on_ready():
+    await tree.sync() #syncronise bot commands
     print(f'We have logged in as {client.user}')
-    
-@client.slash_command()
-async def play(ctx, filename: str):
-    channel = ctx.author.voice.channel
+
+pvolume = 0.2
+
+tree = app_commands.CommandTree(client)
+
+@tree.command(name="ressetmerf", description="reset the merf count, obviously")
+async def resetmerf(interaction):
+    global merf_count
+    merf_count = 0
+    await interaction.response.send_message("Merf count has been reset.")
+
+@tree.command(name="dcmein", description="disconnects you from this channel after [time] minuites")
+async def dcmein(interaction, time: float):
+    if interaction.user.voice:
+        voice_channel = interaction.user.voice.channel
+        #voice_client = interaction.voice_client
+        if voice_channel and True:
+            await interaction.response.send_message(f"Disconnecting {interaction.user.name} in {time} minute(s)...")
+            await asyncio.sleep(time * 60)
+            await interaction.user.move_to(None)
+            await client.get_channel(interaction.channel_id).send(f"{interaction.user.name} has been disconnected from the voice channel.")
+    else:
+        await interaction.response.send_message("You are not in a voice channel.")
+
+
+@tree.command(name="play", description="epic koru play command, supports YT URL or search for video")
+async def play(interaction, ytvideotitle: str):
+    user = interaction.user
+    if 'PooPoo' in [role.name for role in interaction.user.roles]:
+        await interaction.response.send_message('You have the poopoo role!', ephemeral = stfu)
+        return
+    channel = interaction.user.voice.channel
     if channel is None:
-        await ctx.respond("You are not in a voice channel.", ephemeral = stfu)
+        await interaction.response.send_message("You are not in a voice channel.", ephemeral = stfu)
         return
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -32,78 +90,146 @@ async def play(ctx, filename: str):
             'preferredquality': '192',
         }],
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(f'ytsearch1:{filename}', download=False)
-        if not result['entries']:
-            await ctx.send("No video found.")
-            return
-        url = result['entries'][0]['url']
 
-    # Connect to voice channel and play music
-    
+    responsemessage = "Downloading..."
+    isurl = True
+    ytvideotitle = ytvideotitle.replace("https://youtu.be/", "https://www.youtube.com/watch?v=")
+    if not "http" in ytvideotitle:
+        if not "://" in ytvideotitle:
+            ytvideotitle = f'ytsearch1:{ytvideotitle}'
+            isurl = False
+            responsemessage = "Searching..."
+    print(ytvideotitle)
+
+    await interaction.response.send_message(f"{responsemessage}", ephemeral = stfu)
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(f"{ytvideotitle}", download=False)
+        if not isurl:
+            if not result['entries']:
+                await interaction.followup.send("No video found.")
+                return
+        if not isurl:
+            aquiredvideo = result['entries'][0]
+        else:
+            aquiredvideo = result
+        #print(aquiredvideo)
+        url = aquiredvideo['url']
+        webpage_url = aquiredvideo['webpage_url']
+        duration_string = aquiredvideo['duration_string']
+        realduration = aquiredvideo['duration']
+        # Connect to voice channel and play music
+
+    await interaction.followup.send(f"Playing, {duration_string}\n{webpage_url}", ephemeral = stfu)
     if not channel.guild.voice_client:
         await channel.connect()
     voice = channel.guild.voice_client
-    voice.play(discord.FFmpegPCMAudio(url))
-    voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 0.2
+    print(f"{interaction.user} Played {ytvideotitle}.")
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url,before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",options="-vn"))
+    source.volume = pvolume
+    if voice.is_playing():
+        voice.stop()
+    voice.play(source)
+    print("volume = " + str(pvolume))
+    await asyncio.sleep(realduration + 10)
+    print("checking if no sound is playing")
+    if not voice.is_playing():
+        print("disconnecting")
+        voice.stop()
+        vc = discord.utils.get(client.voice_clients, guild=interaction.guild)
+        await vc.disconnect()
+    else:
+        print(f"voice is playing? {voice.is_playing}")
 
-@client.slash_command()
-async def volume(ctx, volume: float):
-    if volume < 0 or volume > 100:
-        await ctx.respond("Volume must be between 0 and 100.", ephemeral = stfu)
+@tree.command(name="volume", description="sets the volume to [volume] %")
+async def volume(interaction, volume: float):
+    user = interaction.user
+    if 'PooPoo' in [role.name for role in interaction.user.roles]:
+        await interaction.response.send_message('You have the poopoo role!', ephemeral = stfu)
         return
-    voice_client = ctx.guild.voice_client
+    if volume < 0 or volume > 100:
+        await interaction.response.send_message("Volume must be between 0 and 100.", ephemeral = stfu)
+        return
+    voice_client = interaction.guild.voice_client
     if not voice_client:
-        await ctx.respond("I am not currently playing any music.", ephemeral = stfu)
+        await interaction.response.send_message("I am not currently playing any music.", ephemeral = stfu)
         return
     voice_client.source.volume = volume/100
-    await ctx.respond(f"Volume set to {volume}%.", ephemeral = stfu)
+    pvolume = volume/100
+    print(f"{interaction.user} set Volume to " + str(pvolume) + ".")
+    await interaction.response.send_message(f"Volume set to {volume}%.", ephemeral = stfu)
     return
 
-@client.slash_command()
-async def join(ctx):
-    channel = ctx.author.voice.channel
-    await ctx.respond("Joining: "+str(ctx.author.voice.channel), ephemeral = stfu)
-    if not channel.guild.voice_client:
-        await channel.connect()
+@tree.command(name="join", description="bot joins your channel")
+async def join(interaction):
+    if interaction.user.voice:
+        user = interaction.user
+        if 'PooPoo' in [role.name for role in interaction.user.roles]:
+            await interaction.response.send_message('You have the poopoo role!', ephemeral = stfu)
+            return
+        print(f"{interaction.user} joined the bot.")
+        await interaction.response.send_message("Joining: "+str(interaction.user.voice.channel), ephemeral = stfu)
+        voice_client = interaction.guild.voice_client
+        if not voice_client:
+            voice_channel = interaction.user.voice.channel
+            await voice_channel.connect()
+    else:
+        await interaction.response.send_message("You are not in a voice channel.")
 
-@client.slash_command()
-async def disconnect(ctx):
-    channel = ctx.author.voice.channel
-    vc = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+@tree.command(name="disconnect", description="bot disconnects from your channel")
+async def disconnect(interaction):
+    user = interaction.user
+    if 'PooPoo' in [role.name for role in interaction.user.roles]:
+        await interaction.response.send_message('You have the poopoo role!', ephemeral = stfu)
+        return
+    print(f"{interaction.user} Disconnected the bot.")
+    channel = interaction.user.voice.channel
+    vc = discord.utils.get(client.voice_clients, guild=interaction.guild)
     voice_client = channel.guild.voice_client
-    await ctx.respond("Disconnected From: "+str(ctx.author.voice.channel), ephemeral = stfu)
+    await interaction.response.send_message("Disconnected From: "+str(interaction.user.voice.channel), ephemeral = stfu)
     voice_client.stop()
     await vc.disconnect()
     print("Bot disconnected")
 
-@client.slash_command()
-async def pause(ctx):
+@tree.command(name="pause", description="pause the current video")
+async def pause(interaction):
+    user = interaction.user
+    if 'PooPoo' in [role.name for role in interaction.user.roles]:
+        await interaction.response.send_message('You have the poopoo role!', ephemeral = stfu)
+        return
     print("resume")
-    voice_client = ctx.guild.voice_client
+    voice_client = interaction.guild.voice_client
     if not voice_client:
-        return await ctx.respond("I am not currently playing any music.", ephemeral = stfu)
+        return await interaction.response.send_message("I am not currently playing any music.", ephemeral = stfu)
     if voice_client.is_paused():
-        return await ctx.respond("The music is already paused.", ephemeral = stfu)
+        return await interaction.response.send_message("The music is already paused.", ephemeral = stfu)
     voice_client.pause()
-    await ctx.respond("Paused the current music.", ephemeral = stfu)
+    await interaction.response.send_message("Paused the current music.", ephemeral = stfu)
 
-@client.slash_command()
-async def stop(ctx):
-    ctx.voice_client.stop()
-    await ctx.respond("stopped.", ephemeral = stfu)
+@tree.command(name="stop", description="stop the current video")
+async def stop(interaction):
+    user = interaction.user
+    if 'PooPoo' in [role.name for role in interaction.user.roles]:
+        await interaction.response.send_message('You have the poopoo role!', ephemeral = stfu)
+        return
+    interaction.guild.voice_client.stop()
+    await interaction.response.send_message("stopped.", ephemeral = stfu)
+    print(f"{interaction.user} stopped playback.")
 
 
-@client.slash_command()
-async def resume(ctx):
+@tree.command(name="resume", description="resume the current video")
+async def resume(interaction):
+    user = interaction.user
+    if 'PooPoo' in [role.name for role in interaction.user.roles]:
+        await interaction.response.send_message('You have the poopoo role!', ephemeral = stfu)
+        return
     print("pasue")
-    voice_client = ctx.guild.voice_client
+    voice_client = interaction.guild.voice_client
     if not voice_client:
-        return await ctx.respond("I am not currently playing any music.", ephemeral = stfu)
+        return await interaction.response.send_message("I am not currently playing any music.", ephemeral = stfu)
     if not voice_client.is_paused():
-        return await ctx.respond("The music is not paused.", ephemeral = stfu)
+        return await interaction.response.send_message("The music is not paused.", ephemeral = stfu)
     voice_client.resume()
-    await ctx.respond("Resumed the current music.", ephemeral = stfu)
+    await interaction.response.send_message("Resumed the current music.", ephemeral = stfu)
 
-client.run('tokenere')
+client.run('') #Not Making This Mistake Again
